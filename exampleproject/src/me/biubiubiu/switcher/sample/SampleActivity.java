775bd6180c72a28ec51fb56b660406c3a16e146c
ufo22940268 <ufo22940268@gmail.com>
2014-01-26 19:08:47 +0800

    first init

---
+++ exampleproject/src/me/biubiubiu/switcher/sample/SampleActivity.java
@@ -0,0 +1,28 @@
+package me.biubiubiu.switcher.sample;
+
+import me.biubiubiu.switcher.sample.R;
+
+import android.util.*;
+import android.widget.*;
+import android.view.*;
+import android.content.*;
+import android.app.*;
+import android.os.*;
+import android.text.*;
+import android.database.*;
+import android.net.*;
+import android.opengl.*;
+import android.graphics.*;
+import android.view.animation.*;
+import android.text.TextUtils;
+
+import java.util.*;
+
+public class SampleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+    }
+}
